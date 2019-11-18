09b3fab3673da4bc758d09680b53c9bacfe7a9c3
Willy Arisky <willy.arisky@gmail.com>
2019-11-18 17:29:35 +0700

    add verify as default

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 // routes for admin
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin', 'AdminController@index')->name('admin');
 });
 // routes for user
-Route::middleware(['auth', 'roles:user'])->group(function () {
+Route::middleware(['auth', 'roles:user', 'verified'])->group(function () {
     Route::get('/user', 'UserController@index')->name('user');
 });
 // routes for both roles
-Route::middleware(['auth', 'roles:admin,user'])->group(function () {
+Route::middleware(['auth', 'roles:admin,user', 'verified'])->group(function () {
     Route::get('/home', 'HomeController@index')->name('home');
 });
